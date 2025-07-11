4ab203351ed5c94dc92e5b53945323a48d148de6
Ruslan <r.petunin@it.ecommpay.com>
2025-07-11 19:30:15 +0300

    Translations

--- src/components/form/PaymentForm.vue
+++ src/components/form/PaymentForm.vue
@@ -1,23 +1,49 @@
 <template>
   <form action="">
     <template v-for="field of paymentMethod.paymentForm.fields" :key="field.name">
-      <PPInput v-if="PaymentMethodFieldType.NUMBER === field.type" type="number" v-model="value" label="Card number" />
-      <PPInput v-else-if="PaymentMethodFieldType.TEXT === field.type" type="text" v-model="value" label="Card number" />
-      <PPInput v-else-if="PaymentMethodFieldType.EMAIL === field.type" type="email" v-model="value" label="Card number" />
-      <PPInput v-else-if="PaymentMethodFieldType.TEL === field.type" type="tel" v-model="value" label="Card number" />
-      <PPInput v-else-if="PaymentMethodFieldType.PASSWORD === field.type" type="password" v-model="value" label="Card number" />
+      <PPInput
+        v-if="PaymentMethodFieldType.NUMBER === field.type"
+        :label="translate(`label_${field.name}`)"
+        type="number"
+        v-model="value"
+      />
+      <PPInput
+        v-else-if="PaymentMethodFieldType.TEXT === field.type"
+        :label="translate(`label_${field.name}`)"
+        type="text"
+        v-model="value"
+      />
+      <PPInput
+        v-else-if="PaymentMethodFieldType.EMAIL === field.type"
+        :label="translate(`label_${field.name}`)"
+        type="email"
+        v-model="value"
+      />
+      <PPInput
+        v-else-if="PaymentMethodFieldType.TEL === field.type"
+        :label="translate(`label_${field.name}`)"
+        type="tel"
+        v-model="value"
+      />
+      <PPInput
+        v-else-if="PaymentMethodFieldType.PASSWORD === field.type"
+        :label="translate(`label_${field.name}`)"
+        type="password"
+        v-model="value"
+      />
     </template>
   </form>
 </template>
 
 <script setup lang="ts">
-import { PaymentMethodFieldType } from "orchestrator-pp-core";
-import type { PaymentMethod } from "orchestrator-pp-core";
+import { PaymentMethodFieldType } from 'orchestrator-pp-core'
+import type { PaymentMethod, Translate } from "orchestrator-pp-core";
 import { PPInput } from "orchestrator-pp-vue-ui-kit";
 import { ref } from 'vue'
 
 defineProps<{
-  paymentMethod: PaymentMethod
+  paymentMethod: PaymentMethod,
+  translate: Translate
 }>();
 
 const value = ref<string>('');
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as PaymentForm } from './form/PaymentForm.vue';
+export { default as LanguageSelect } from './controls/LanguageSelect.vue';
