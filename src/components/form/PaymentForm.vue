2c1e2e4464b4323337eaf281797d26b031481477
Ruslan <r.petunin@it.ecommpay.com>
2025-07-17 11:59:46 +0300

    Payment form validation and sending

--- src/components/form/PaymentForm.vue
+++ src/components/form/PaymentForm.vue
@@ -1,22 +1,27 @@
 <template>
-  <form action="" @submit="handleSubmit">
+  <form action="" ref="formRef" @submit.prevent="handleSubmit" @input="handleInput">
     <template v-for="field of paymentMethod.paymentForm.fields" :key="field.name">
       <PPInput
         v-if="isPPInputType(field.type)"
-        :label="translate(`label_${field.name}`)"
+        :label="translate(`l_${field.name}`)"
         :type="field.type"
         :name="field.name"
+        @blur="touched.push(field.name)"
+        :error="errors[field.name]"
         class="pp-input"
       />
     </template>
-    <PPButton class="pp-button">{{ translate(`btn_pay`) }}</PPButton>
+    <PPButton class="pp-button" :disabled="!formValidationResult.isValid">
+      {{ translate(`b_pay`) }}
+    </PPButton>
   </form>
 </template>
 
 <script setup lang="ts">
 import type { Translate } from "orchestrator-pp-core";
-import type { PaymentMethod } from "orchestrator-pp-payment-method";
+import type { PaymentMethod, FormValidationResult } from "orchestrator-pp-payment-method";
 import { PPInput } from "orchestrator-pp-vue-ui-kit";
+import { ref, onMounted, computed } from 'vue'
 
 const props = defineProps<{
   paymentMethod: PaymentMethod,
@@ -27,29 +32,77 @@ const emit = defineEmits<{
   (event: 'pay', data: Record<string, unknown>): void;
 }>();
 
+const touched = ref<string[]>([]);
+const formValidationResult = ref<FormValidationResult>({ isValid: false, errors: {} });
+const formRef = ref<HTMLFormElement>();
+
+const errors = computed<Record<string, string>>(
+  () => {
+    const result: Record<string, string> = {};
+
+    if (!formValidationResult.value.isValid) {
+      for (const fieldName of Object.keys(formValidationResult.value.errors)) {
+        const errors = formValidationResult.value.errors[fieldName];
+
+        if (errors && touched.value.includes(fieldName)) {
+          const firstErrorRuleName = Object.keys(errors)[0];
+
+          result[fieldName] = props.translate(
+            `ve_${firstErrorRuleName}`,
+            {
+              name: props.translate(`l_${fieldName}`)
+            }
+          );
+        }
+      }
+    }
+
+    return result;
+  }
+);
+
 function isPPInputType(type: string): type is 'text' | 'number' | 'email' | 'password' | 'tel' {
   return ['text', 'number', 'email', 'password', 'tel'].includes(type);
 }
 
-async function handleSubmit(event: Event) {
-  event.preventDefault();
-
-  const form = event.target as HTMLFormElement;
-  const formData = new FormData(form);
-
+function getFormData(): Record<string, unknown> {
   const data: Record<string, unknown> = {};
-  formData.forEach((value, key) => {
-    data[key] = value;
-  });
 
-  await props.paymentMethod.paymentForm.onSubmit?.(data);
-  emit('pay', data);
+  if (!formRef.value) {
+    return data;
+  } else {
+    const formData = new FormData(formRef.value);
+
+    formData.forEach((value, key) => {
+      data[key] = value
+    });
+
+    return data;
+  }
 }
+
+async function validateForm(): Promise<void> {
+  formValidationResult.value.isValid = false;
+  formValidationResult.value = await props.paymentMethod.paymentForm.validate(getFormData());
+}
+
+async function handleInput(): Promise<void> {
+  await validateForm();
+}
+
+async function handleSubmit() {
+  const formData = getFormData();
+
+  await props.paymentMethod.paymentForm.onSubmit(formData);
+  emit('pay', formData);
+}
+
+onMounted(validateForm);
 </script>
 
 <style scoped>
 .pp-input:not(:last-of-type) {
-  margin-bottom: var(--pp-gap-md);
+  margin-bottom: var(--pp-gap-sm);
 }
 
 .pp-button {
