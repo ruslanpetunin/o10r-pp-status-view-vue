d86acca849ca1f346116934c073fc602d98f2dce
Ruslan <r.petunin@it.ecommpay.com>
2025-07-18 15:18:11 +0300

    PaymentStatusView

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as PaymentForm } from './form/PaymentForm.vue';
 export { default as LanguageSelect } from './controls/LanguageSelect.vue';
+export { default as PaymentStatusView } from './view/PaymentStatusView.vue';
