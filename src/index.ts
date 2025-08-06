3aec1f97a24278ddef8ec0d299cc4a5c79814d54
Ruslan <r.petunin@it.ecommpay.com>
2025-08-06 16:48:39 +0300

    Change plugin connect function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { App } from 'vue';
+import type { App, FunctionPlugin } from 'vue';
 import PpUiKit from 'orchestrator-pp-vue-ui-kit';
 import * as components from './components';
 
@@ -8,15 +8,13 @@ import './style.css';
 export * from './components';
 export * from 'orchestrator-pp-vue-ui-kit';
 
-export default {
-    install(app: App) {
-        app.use(PpUiKit);
+const connect: FunctionPlugin = (app: App) => {
+  app.use(PpUiKit);
 
-        for (const key in components) {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            app.component(key, (components as any)[key]);
-        }
+  for (const key in components) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    app.component(key, (components as any)[key]);
+  }
+}
 
-        return app;
-    }
-};
+export default connect;
